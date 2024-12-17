15378d44955550fe282969af485312abf28224b5
Nandhini8848 <nandhini@8848digital.com>
2024-12-17 12:28:50 +0530

    fix: pre-commit changes

--- inventory_dimensions/inventory_dimensions/customizations/delivery_note/api_calls/api_call_functions.py
+++ inventory_dimensions/inventory_dimensions/customizations/delivery_note/api_calls/api_call_functions.py
@@ -13,29 +13,30 @@ def get_shelf_qty(item_code, warehouse, shelf):
 
 
 def get_rack_list(item_code, warehouse):
-	final_list=[]
+	final_list = []
 	rack_qty = frappe.get_all(
 		"Stock Ledger Entry",
 		filters={"item_code": item_code, "warehouse": warehouse},
 		group_by="rack",
-		fields=["sum(actual_qty) as total","rack"],
+		fields=["sum(actual_qty) as total", "rack"],
 	)
 
 	for i in rack_qty:
-		if i["total"] >0:
+		if i["total"] > 0:
 			final_list.append(i["rack"])
 	return final_list
 
+
 def get_shelf_list(item_code, warehouse):
-	final_list=[]
+	final_list = []
 	shelf_qty = frappe.get_all(
 		"Stock Ledger Entry",
 		filters={"item_code": item_code, "warehouse": warehouse},
 		group_by="shelf",
-		fields=["sum(actual_qty) as total","shelf"],
+		fields=["sum(actual_qty) as total", "shelf"],
 	)
 
 	for i in shelf_qty:
-		if i["total"] >0:
+		if i["total"] > 0:
 			final_list.append(i["shelf"])
 	return final_list
--- inventory_dimensions/inventory_dimensions/customizations/delivery_note/delivery_note.py
+++ inventory_dimensions/inventory_dimensions/customizations/delivery_note/delivery_note.py
@@ -1,20 +1,19 @@
 import frappe
 
 from inventory_dimensions.inventory_dimensions.customizations.delivery_note.api_calls.api_call_functions import (
-	get_rack_list,
-	get_shelf_list,
-	get_shelf_qty,
-)
+    get_rack_list, get_shelf_list, get_shelf_qty)
 
 
 @frappe.whitelist()
 def get_total_shelf_qty(item_code, warehouse, shelf):
 	return get_shelf_qty(item_code, warehouse, shelf)
 
+
 @frappe.whitelist()
 def _get_rack_list(item_code, warehouse):
-    return get_rack_list(item_code, warehouse)
+	return get_rack_list(item_code, warehouse)
+
 
 @frappe.whitelist()
 def _get_shelf_list(item_code, warehouse):
-    return get_shelf_list(item_code, warehouse)
+	return get_shelf_list(item_code, warehouse)
