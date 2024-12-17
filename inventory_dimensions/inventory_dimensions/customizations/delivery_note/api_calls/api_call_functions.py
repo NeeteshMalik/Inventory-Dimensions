57dcc95a3782c0acf498248e41fc0c1534bd0b0f
Nandhini8848 <nandhini@8848digital.com>
2024-12-17 10:38:33 +0530

    feat: add rack and shelf field filter

--- inventory_dimensions/inventory_dimensions/customizations/delivery_note/api_calls/api_call_functions.py
+++ inventory_dimensions/inventory_dimensions/customizations/delivery_note/api_calls/api_call_functions.py
@@ -10,3 +10,32 @@ def get_shelf_qty(item_code, warehouse, shelf):
 
 	if shelf_qty:
 		return shelf_qty
+
+
+def get_rack_list(item_code, warehouse):
+	final_list=[]
+	rack_qty = frappe.get_all(
+		"Stock Ledger Entry",
+		filters={"item_code": item_code, "warehouse": warehouse},
+		group_by="rack",
+		fields=["sum(actual_qty) as total","rack"],
+	)
+
+	for i in rack_qty:
+		if i["total"] >0:
+			final_list.append(i["rack"])
+	return final_list
+
+def get_shelf_list(item_code, warehouse):
+	final_list=[]
+	shelf_qty = frappe.get_all(
+		"Stock Ledger Entry",
+		filters={"item_code": item_code, "warehouse": warehouse},
+		group_by="shelf",
+		fields=["sum(actual_qty) as total","shelf"],
+	)
+
+	for i in shelf_qty:
+		if i["total"] >0:
+			final_list.append(i["shelf"])
+	return final_list
--- inventory_dimensions/inventory_dimensions/customizations/delivery_note/delivery_note.js
+++ inventory_dimensions/inventory_dimensions/customizations/delivery_note/delivery_note.js
@@ -31,3 +31,58 @@ frappe.ui.form.on("Delivery Note Item", {
     }
   },
 });
+frappe.ui.form.on('Delivery Note', {
+  refresh: function (frm) {
+    rack_filter_query(frm);
+	shelf_filter_query(frm);
+
+  }
+});
+
+
+function rack_filter_query(frm) {
+	let rack_filter = [];
+	frm.set_query( "rack","items", function (doc, cdt, cdn) {
+		var d = locals[cdt][cdn];
+		frappe.call({
+			method: "inventory_dimensions.inventory_dimensions.customizations.delivery_note.delivery_note._get_rack_list",
+			async: false,
+			args: {
+				item_code: d.item_code,
+        warehouse:d.warehouse
+			},
+			callback(r) {
+				rack_filter = r.message;
+			},
+		});
+		return {
+			filters: {
+				name: ["in", rack_filter],
+			},
+		};
+	});
+	
+}
+
+function shelf_filter_query(frm) {
+	let shelf_filter = [];
+	frm.set_query( "shelf","items", function (doc, cdt, cdn) {
+		var d = locals[cdt][cdn];
+		frappe.call({
+			method: "inventory_dimensions.inventory_dimensions.customizations.delivery_note.delivery_note._get_shelf_list",
+			async: false,
+			args: {
+				item_code: d.item_code,
+        warehouse:d.warehouse
+			},
+			callback(r) {
+				shelf_filter = r.message;
+			},
+		});
+		return {
+			filters: {
+				name: ["in", shelf_filter],
+			},
+		};
+	});
+}
