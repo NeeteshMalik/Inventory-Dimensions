2269fffe8a239154cc3b6f76070dc979b6c009b6
Nandhini8848 <nandhini@8848digital.com>
2024-12-17 11:53:14 +0530

    fix: pre-commit changes

--- inventory_dimensions/inventory_dimensions/customizations/delivery_note/delivery_note.js
+++ inventory_dimensions/inventory_dimensions/customizations/delivery_note/delivery_note.js
@@ -1,55 +1,47 @@
 frappe.ui.form.on("Delivery Note Item", {
-  shelf: function (frm, cdt, cdn) {
-    let row = locals[cdt][cdn];
-    if (row.warehouse && row.item_code && row.shelf) {
-      frappe.call({
-        method:
-          "inventory_dimensions.inventory_dimensions.customizations.delivery_note.delivery_note.get_total_shelf_qty",
-        args: {
-          item_code: row.item_code,
-          warehouse: row.warehouse,
-          shelf: row.shelf,
-        },
-        callback: function (r) {
-          if (r.message) {
-            frappe.model.set_value(
-              cdt,
-              cdn,
-              "custom_shelf_qty",
-              r.message[0]["total"]
-            );
-          }
-        },
-      });
-    } else {
-      frappe.model.set_value(
-        cdt,
-        cdn,
-        "custom_shelf_qty",
-       0
-      );
-    }
-  },
+	shelf: function (frm, cdt, cdn) {
+		let row = locals[cdt][cdn];
+		if (row.warehouse && row.item_code && row.shelf) {
+			frappe.call({
+				method: "inventory_dimensions.inventory_dimensions.customizations.delivery_note.delivery_note.get_total_shelf_qty",
+				args: {
+					item_code: row.item_code,
+					warehouse: row.warehouse,
+					shelf: row.shelf,
+				},
+				callback: function (r) {
+					if (r.message) {
+						frappe.model.set_value(
+							cdt,
+							cdn,
+							"custom_shelf_qty",
+							r.message[0]["total"]
+						);
+					}
+				},
+			});
+		} else {
+			frappe.model.set_value(cdt, cdn, "custom_shelf_qty", 0);
+		}
+	},
 });
-frappe.ui.form.on('Delivery Note', {
-  refresh: function (frm) {
-    rack_filter_query(frm);
-	shelf_filter_query(frm);
-
-  }
+frappe.ui.form.on("Delivery Note", {
+	refresh: function (frm) {
+		rack_filter_query(frm);
+		shelf_filter_query(frm);
+	},
 });
-
 
 function rack_filter_query(frm) {
 	let rack_filter = [];
-	frm.set_query( "rack","items", function (doc, cdt, cdn) {
+	frm.set_query("rack", "items", function (doc, cdt, cdn) {
 		var d = locals[cdt][cdn];
 		frappe.call({
 			method: "inventory_dimensions.inventory_dimensions.customizations.delivery_note.delivery_note._get_rack_list",
 			async: false,
 			args: {
 				item_code: d.item_code,
-        warehouse:d.warehouse
+				warehouse: d.warehouse,
 			},
 			callback(r) {
 				rack_filter = r.message;
@@ -61,19 +53,18 @@ function rack_filter_query(frm) {
 			},
 		};
 	});
-	
 }
 
 function shelf_filter_query(frm) {
 	let shelf_filter = [];
-	frm.set_query( "shelf","items", function (doc, cdt, cdn) {
+	frm.set_query("shelf", "items", function (doc, cdt, cdn) {
 		var d = locals[cdt][cdn];
 		frappe.call({
 			method: "inventory_dimensions.inventory_dimensions.customizations.delivery_note.delivery_note._get_shelf_list",
 			async: false,
 			args: {
 				item_code: d.item_code,
-        warehouse:d.warehouse
+				warehouse: d.warehouse,
 			},
 			callback(r) {
 				shelf_filter = r.message;
