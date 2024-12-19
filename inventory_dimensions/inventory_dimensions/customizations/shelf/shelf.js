9509ba29d05ca1907ab379bdc9f7a866dd543ab1
Nandhini8848 <nandhini@8848digital.com>
2024-12-19 14:22:25 +0530

    feat: shelf and rack doctype moved into custom app

--- inventory_dimensions/inventory_dimensions/customizations/shelf/shelf.js
+++ inventory_dimensions/inventory_dimensions/customizations/shelf/shelf.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on("Shelf", {
-	warehouse: function (frm) {
-        if (frm.doc.warehouse) {
-            // Set a dynamic filter on the Rack field based on the selected Warehouse
+    warehouse: function (frm){
+        if(frm.doc.warehouse){
             frm.set_query('rack', function () {
                 return {
                     filters: {
@@ -9,15 +8,17 @@ frappe.ui.form.on("Shelf", {
                     }
                 };
             });
-
-            // Clear the Rack field when the Warehouse changes
-            frm.set_value('rack', null);
-        } else {
-            // Clear Rack field if Warehouse is cleared
-            frm.set_query('rack', function () {
-                return {};
-            });
-            frm.set_value('rack', null);
         }
-    }
+        else{
+            frm.set_query('rack', function () {
+                return {
+                    filters: {
+                        name: ["in",[]]
+                    }
+                };
+            });
+
+        }
+       
+    },
 });
