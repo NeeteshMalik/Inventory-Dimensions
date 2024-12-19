5997b93ca8319591dcf8296de3570dd64353c61b
Nandhini8848 <nandhini@8848digital.com>
2024-12-19 13:05:13 +0530

    fix: changes in shelf filter script

--- inventory_dimensions/inventory_dimensions/customizations/shelf/shelf.js
+++ inventory_dimensions/inventory_dimensions/customizations/shelf/shelf.js
@@ -1,23 +1,23 @@
 frappe.ui.form.on("Shelf", {
 	warehouse: function (frm) {
-		if (frm.doc.warehouse) {
-			// Set a dynamic filter on the Rack field based on the selected Warehouse
-			frm.set_query("rack", function () {
-				return {
-					filters: {
-						warehouse: frm.doc.warehouse, // Only show racks linked to the selected warehouse
-					},
-				};
-			});
+        if (frm.doc.warehouse) {
+            // Set a dynamic filter on the Rack field based on the selected Warehouse
+            frm.set_query('rack', function () {
+                return {
+                    filters: {
+                        warehouse: frm.doc.warehouse // Only show racks linked to the selected warehouse
+                    }
+                };
+            });
 
-			// Clear the Rack field when the Warehouse changes
-			frm.set_value("rack", null);
-		} else {
-			// Clear Rack field if Warehouse is cleared
-			frm.set_query("rack", function () {
-				return {};
-			});
-			frm.set_value("rack", null);
-		}
-	},
+            // Clear the Rack field when the Warehouse changes
+            frm.set_value('rack', null);
+        } else {
+            // Clear Rack field if Warehouse is cleared
+            frm.set_query('rack', function () {
+                return {};
+            });
+            frm.set_value('rack', null);
+        }
+    }
 });
